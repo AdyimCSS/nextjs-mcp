ff01deef5a5d46773841960ba8a0baf65f9bd008
AdyimCSS <trirong_berm@hotmail.com>
2025-11-03 15:59:30 +0700

    create figma

---
+++ components/berm/button.figma.tsx
@@ -0,0 +1,26 @@
+/**
+ * @figmaId <FIGMA_COMPONENT_ID>
+ * @component Button
+ */
+import * as React from "react";
+import { Button } from "../ui/button";
+
+export default function ButtonFigma(props: {
+  variant?: "default" | "destructive" | "outline" | "secondary" | "ghost" | "link";
+  size?: "default" | "sm" | "lg" | "icon";
+  children?: React.ReactNode;
+  disabled?: boolean;
+  asChild?: boolean;
+}) {
+  return (
+    <Button
+      variant={props.variant}
+      size={props.size}
+      disabled={props.disabled}
+      asChild={props.asChild}
+    >
+      {props.children}
+    </Button>
+  );
+}
+
