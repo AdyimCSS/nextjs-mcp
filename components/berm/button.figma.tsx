1c6635de7df7d09bc2174293800ff6bedb9bc98a
AdyimCSS <trirong_berm@hotmail.com>
2025-11-03 16:35:44 +0700

    update code

--- components/berm/button.figma.tsx
+++ components/berm/button.figma.tsx
@@ -1,7 +1,5 @@
 /**
  * @figmaId 21:9
- * @component Button
- * @figmaLink https://www.figma.com/design/J2LLnfBQi1MCjF2DREqdrD/nextjs-mcp?node-id=21-9&t=lL9f74d9UQgTzOeY-4
  */
 import * as React from "react";
 import { Button } from "../ui/button";
